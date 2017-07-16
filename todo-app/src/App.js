e28bec43e774e63e33442034875cc8f127e421a0
Karl <kturrell@hotmail.com>
2017-07-16 12:20:17 +0100

    Throttle saveState using expensive serialize call

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -7,6 +7,7 @@ import VisibleTodoList from './VisibleTodoList'
 import Footer from './Footer'
 import todoAppReducer from './reducers'
 import { loadState, saveState } from './localStorage'
+import throttle from 'lodash/throttle'
 import './App.css'
 
 // Root container component
@@ -26,11 +27,11 @@ const store = createStore(
     todoAppReducer,
     persistedState
 )
-store.subscribe(() => {
+store.subscribe(throttle(() => {
     saveState({
         todos: store.getState().todos
     })
-})
+}, 1000))
 
 // Renderer
 ReactDOM.render(
