a7a8c907d97d15568e1839f3b1a8497909b6665c
Karl <kturrell@hotmail.com>
2017-07-19 18:29:41 +0100

    Add fake in-memory backend and test

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import Root from './components/Root'
 import configureStore from './configureStore'
 import './App.css'
+import { fetchTodos } from './api'
+
+fetchTodos('all').then(todos =>
+    console.log(todos)
+)
 
 const store = configureStore()
 
