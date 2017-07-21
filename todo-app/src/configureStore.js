dbabb83f7e611fb1e43351329ee1c754f99a2ab3
Karl <kturrell@hotmail.com>
2017-07-21 07:44:25 +0100

    Replace middleware with existing libraries

--- todo-app/src/configureStore.js
+++ todo-app/src/configureStore.js
@@ -1,48 +1,20 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
+import promise from 'redux-promise'
 import todoAppReducer from './reducers/reducers'
-
-// Dispatch logging
-const logger = (store) => (next) => {
-    if (!console.group) {
-        return next
-    }
-
-    return (action) => {
-        console.group(action.type)
-        console.log('%c prev state', 'color: gray', store.getState())
-        console.log('%c action', 'color: blue', action)
-        const returnValue = next(action)
-        console.log('%c next state', 'color: green', store.getState())
-        console.groupEnd(action.type)
-        return returnValue
-    }
-}
-
-const promise = (store) => (next) => (action) => {
-    if (typeof(action.then) === 'function') {
-        return action.then(next)
-    }
-    return next(action)
-}
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-    middlewares.slice().reverse().forEach(middleware =>
-        store.dispatch = middleware(store)(store.dispatch)
-    )
-}
 
 // Create augmented store
 const configureStore = () => {
-    const store = createStore(todoAppReducer)
     const middlewares = [promise]
 
     if (process.env.NODE_ENV !== 'production') {
-        middlewares.push(logger)
+        middlewares.push(createLogger())
     }
 
-    wrapDispatchWithMiddlewares(store, middlewares)
-
-    return store
+    return createStore(
+        todoAppReducer,
+        applyMiddleware(...middlewares)
+    )
 }
 
 export default configureStore
