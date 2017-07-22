9612f68a91036c72a8d9a3ddf0f6974b93d9951c
Karl <kturrell@hotmail.com>
2017-07-22 16:04:59 +0100

    addTodo thunk with fake API endpoint, updates lists correctly

--- todo-app/src/api.js
+++ todo-app/src/api.js
@@ -21,10 +21,6 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
 export const fetchTodos = (filter) =>
     delay(500).then(() => {
-        if (Math.random() > 0.5) {
-            throw new Error('Boom!')
-        }
-
         switch (filter) {
             case 'all':
                 return fakeDatabase.todos
@@ -37,3 +33,21 @@ export const fetchTodos = (filter) =>
         }
     })
 
+
+export const addTodo = (text) =>
+    delay(500).then(() => {
+        const todo = {
+            id: v4(),
+            text,
+            completed: false
+        }
+        fakeDatabase.todos.push(todo)
+        return todo
+    })
+
+export const toggleTodo = (id) =>
+    delay(500).then(() => {
+        const todo = fakeDatabase.todos.find(t => t.id === id)
+        todo.completed = !todo.completed
+        return todo
+    })
